51c16d048a3820b75ab10f29b24ee0e491d749ac
Daria Chorenza <76909@student.ue.poznan.pl>
2023-04-06 12:30:14 +0200

    Dodany prostokat

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -8,3 +8,13 @@ pole = a * a
 # obwód kwadratu wynosi 40, a pole 100.
 print("Obwod kwadratu wynosi " + str(obwod) + ", a pole " + str(pole) + ".")
 
+
+#prostokat 
+
+a = 10
+b = 5 
+
+obwodp = (a * 2) + (b * 2)
+polep = a * b
+print("Obwód prostokąta wynosi " + str(obwodp) + ", a pole " + str(polep) + ".")
+

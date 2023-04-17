c04a769984160aaa931294a3a72a480cab666c6f
Daria Chorenza <76909@student.ue.poznan.pl>
2023-04-17 18:51:25 +0200

    Zadanie1.3

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -13,3 +13,12 @@ print(komunikat)
 student = input("Wpisz swoje imię: ")
 print("Hello " + student)
 
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci 
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = len(studenci)
+print("Liczba studentów wynosi:", liczba_studentow)
+
